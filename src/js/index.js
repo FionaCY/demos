71d8983f24db7d3facb761177130f05bc1101c66
cheryl- <cheryl-@chenyedeMBP.lan>
2017-10-05 13:59:28 +0800

    add vue operation

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,20 @@
 // import $ from 'jquery'
-// const vue = require('vue')
+import Vue from 'vue/dist/vue.common.js'
+
+new Vue({
+  el: '#login',
+  data: {
+    inputtext: {}
+  },
+  methods: {
+    btn: function () {
+      console.log('1')
+      if (!this.inputtext.name) {
+        console.log('email地址不能为空')
+      } else if (!this.inputtext.password) {
+        console.log('密码不能为空')
+      } else {
+      }
+    }
+  }
+})
