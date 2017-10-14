bd3264fa2279bfc088e61cf288df166553dcf65b
chenye01 <1343566210@qq.com>
2017-10-14 17:12:28 +0800

    .

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -34,10 +34,7 @@ module.exports = {
 				test: /\.(png|jpg|gif)$/,
 				use: [
 					{
-						loader: 'url-loader',
-						options: {
-							limit: 8192
-						}
+						loader: 'url-loader'
 					}
 				]
 			},
